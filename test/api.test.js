65ea8a243c191e137b6cb9e68d0a5e1be6811b99
Masih Niaz <masihniaz@yahoo.com>
2021-07-17 01:36:38 +0800

    Added Signup Tests

--- test/api.test.js
+++ test/api.test.js
@@ -4,11 +4,63 @@ const request = require("supertest");
 const expect = chai.expect;
 
 describe("API Tests", () => {
-  describe("Login", (req, res) => {
+  xdescribe("Signup", (req, res) => {
+    it("Should be able to sign up with correct information", (done) => {
+      const payload = {
+        username: "masih",
+        email: "masihniazz@gmail.com",
+        password: "password",
+      };
+      request(app)
+        .post("/api/signup")
+        .send(payload)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body).to.have.property("id");
+          expect(res.body).to.have.property("username", payload.username);
+          expect(res.body).to.have.property("email", payload.email);
+          done();
+        });
+    });
+
+    it("Should respond with error if body parameters are missing", (done) => {
+      const payload = {};
+      request(app)
+        .post("/api/signup")
+        .send(payload)
+        .expect(401)
+        .end((err, res) => {
+          expect(res.body).to.have.property("errors");
+          done();
+        });
+    });
+
+    it("Should respond with error if username or email exist", (done) => {
+      const payload = {
+        username: "masih",
+        email: "masihniazz@gmail.com",
+        password: "password",
+      };
+      request(app)
+        .post("/api/signup")
+        .send(payload)
+        .expect(401)
+        .end((err, res) => {
+          expect(res.body).to.have.property(
+            "error",
+            `User with email address: "${payload.email}" or username: "${payload.username}" already exists`
+          );
+          done();
+        });
+    });
+  });
+
+  xdescribe("Login", (req, res) => {
     it("Should be able to login with correct credentials", (done) => {
+      const payload = { username: "demo1", password: "password" };
       request(app)
         .post("/api/login")
-        .send({ username: "demo1", password: "password" })
+        .send(payload)
         .expect(200)
         .end((err, res) => {
           expect(res.body).to.have.property("access_token");
@@ -16,9 +68,10 @@ describe("API Tests", () => {
         });
     });
     it("Should not be able to login with incorrect credentials", (done) => {
+      const payload = { username: "demo1", password: "wrongpassword" };
       request(app)
         .post("/api/login")
-        .send({ username: "demo1", password: "wrongpassword" })
+        .send(payload)
         .expect(401)
         .end((err, res) => {
           expect(res.body).to.have.property(
@@ -29,9 +82,10 @@ describe("API Tests", () => {
         });
     });
     it("Should respond with error if body parameters are missing", (done) => {
+      const payload = {};
       request(app)
         .post("/api/login")
-        .send({})
+        .send(payload)
         .expect(401)
         .end((err, res) => {
           expect(res.body).to.have.property("errors");
