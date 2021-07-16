3f4db8eab675f00db16660a78fc628be7a4dabe8
Masih Niaz <masihniaz@yahoo.com>
2021-07-16 22:59:14 +0800

    Fix - Revert Back the Sequelize Config File to Original Structure

--- models/index.js
+++ models/index.js
@@ -6,20 +6,17 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
-const dbConfig = config.database;
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
-} else {
-  sequelize = new Sequelize(
-    dbConfig.database,
-    dbConfig.username,
-    dbConfig.password,
-    dbConfig
-  );
-}
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+  }
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
