16016b9b4d19f7ad6e43b89217857564d232b235
Masih Niaz <masihniaz@yahoo.com>
2021-07-18 22:24:55 +0800

    Added React Build to the Public Folder & Setup Express Static

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require("passport");
 const { User } = require("./db/models");
 const cors = require("cors");
+const path = require("path");
 const { configurePassport } = require("./middlewares");
 const authRoutes = require("./routes/auth");
 const protectedRoutes = require("./routes/protected");
@@ -15,6 +16,9 @@ const jwtOpt = { session: false };
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public"), { redirect: true }));
+
 app.get("/api", (req, res) =>
   res.status(200).json({ version: require("./package.json").version })
 );
